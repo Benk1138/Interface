bd75aa56998d219647794d871dc253c2b6d32fbe
benjamin kunkel <benjamink1125@gmail.com>
2022-11-03 08:58:04 -0700

    initial commit

---
+++ Interfaces/SUV.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace Interfaces
+{
+    public class SUV : Ivehicle, ICompany 
+    {
+        public SUV()
+        {
+        }
+
+        public double EngineSize { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int SeatCount { get; set; }
+        public string Name { get; set; }
+        public string Motto { get; set; }
+        public bool HasChangedGears { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} now driving forward");
+        }
+
+
+        public void Reverse()
+        {
+            if (HasChangedGears == true)
+            {
+
+            }
+            Console.WriteLine($"{GetType().Name} now in reverse");
+            HasChangedGears = false;
+
+        }
+        else
+
+        {
+          Console.WriteLine("Can't reverse until we change gears");
+        }
+
+
+    public void Park()
+
+    {
+
+
+        if (HasChangedGears == true)
+        {
+
+        }
+        Console.WriteLine($"{GetType().Name} now in park");
+        HasChangedGears = false;
+
+    }
+        else
+
+        {
+          Console.WriteLine("Can't park until we change gears");
+        }
+Console.WriteLine();
+
+public void ChangeGears(bool hasChanged)
+{
+    HasChangedGears = isChanged;
+}
+        }
+
+        public bool ChangeGears(bool isChanged)
+{
+    throw new NotImplementedException();
+}
+    }
+
+}
+    }
+}
+
